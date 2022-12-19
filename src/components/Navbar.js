233330da825329297f4b05c8eda4807fc24da099
Kendell Andrews <kendellandrews2@gmail.com>
2022-12-18 19:22:37 -0700

    resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ export default function Navbar() {
               <a href="#skills" className="md:mr-5 hover:text-white">
                 Skills
               </a>
-              <a href="https://drive.google.com/file/d/1WBRXZeLitKtSZeRz-xuG8we1apwBNt5h/view?usp=sharing" target='_blank' rel="noopener noreferrer" className="md:mr-5 hover:text-white">
+              <a href="./public/Kendell Andrews Resume.pdf" target='_blank' rel="noopener noreferrer" className="md:mr-5 hover:text-white">
                 Resumé
               </a>
             </nav>
