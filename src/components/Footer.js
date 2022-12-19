00666201252473de3e476ff357765eb419838066
Kendell Andrews <kendellandrews2@gmail.com>
2022-12-18 19:20:28 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,13 +53,9 @@ export default function Footer() {
       <div className="container mx-auto flex px-10 py-16 md:flex-row flex-col items-center justify-center">
         <div className="lg:flex-grow flex flex-col items-center mb-16 md:mb-0 text-center xl:w-1/2 lg:w-1/2 md:w-3/4 sm:w-full">
             <div className="flex flex-wrap flex-row justify-between xl:w-1/2 lg:w-1/2 md:w-3/4 sm:w-full">
-                <SocialIcon url="https://www.linkedin.com/in/matthewrapp-/" target='_blank' bgColor='transparent' fgColor='#fff' />
-                <SocialIcon url="https://github.com/matthewrapp" target='_blank' bgColor='transparent' fgColor='#fff'  />
-                <SocialIcon url="https://www.instagram.com/matthewrapp" target='_blank' bgColor='transparent' fgColor='#fff'  />
-                <SocialIcon url="https://www.facebook.com/matthewrapp05" target='_blank' bgColor='transparent' fgColor='#fff'  />
-                <SocialIcon url="https://www.behance.net/matthewrapp" target='_blank' bgColor='transparent' fgColor='#fff'  />
-                <SocialIcon url="https://www.soundcloud.com/luckycoinmusic" target='_blank' bgColor='transparent' fgColor='#fff'  />
-                <SocialIcon url="https://open.spotify.com/artist/6y5aoRJCoGqWkIAcg6dsQc?si=93HIAveXSzuNXDVbL1aTUg&dl_branch=1" target='_blank' bgColor='transparent' fgColor='#fff'/>
+                <SocialIcon url="https://www.linkedin.com/in/kendellandrews/" target='_blank' bgColor='transparent' fgColor='#fff' />
+                <SocialIcon url="https://github.com/kendellandrews" target='_blank' bgColor='transparent' fgColor='#fff'  />
+                <SocialIcon url="https://www.instagram.com/kendell.andrews" target='_blank' bgColor='transparent' fgColor='#fff'  />
             </div>
             <div className='mt-6'>
                 <h3 className="title-font text-gray-200 mb-3 pl-2">&copy; {getMonth(new Date().getMonth()) + ' ' + new Date().getFullYear()} | Kendell Andrews</h3>
